3e9e237fc0f5cb92c4129d295efecada0bbaa54b
screamz2k <screamz2k@users.noreply.github.com>
2023-05-09 16:58:04 +0200

    redirect to dashboard

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
 import MainFrame from "./components/mainframe";
 import LoginForm from "./pages/login";
 import DashBoard from "./pages/dashboard";
@@ -21,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: "/dashboard",
     element: <MainFrame body={<DashBoard />} />,
+  },
+  {
+    path: "/",
+    loader : () => redirect("/dashboard"),
   },
   {
     path: "/operations",
